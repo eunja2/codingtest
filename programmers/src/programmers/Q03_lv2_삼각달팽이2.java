60a8a239da59704bb53fa824aa012e4a842d4518
eunja2 <ej0514@hanmail.net>
2023-06-18 18:05:00 +0900

    03 삼각달팽이 2

---
+++ programmers/src/programmers/Q03_lv2_삼각달팽이2.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Q03_lv2_삼각달팽이2 {
+	private static final int[] dx = {0, 1, -1}; 
+	private static final int[] dy = {1, 0, -1}; 
+	
+	public int[] solution(int n) {
+		 	int[][] triangle = new int[n][n];
+		 	int x = 0;
+		 	int y = 0;
+		 	int d = 0;
+		 	
+	        int[] answer = {};
+	        return answer;
+	}
+}
