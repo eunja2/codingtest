e66d653cf0c08e9ebc9173a1a1963082d9697696
eunja2 <ej0514@hanmail.net>
2023-06-18 17:30:02 +0900

    삼각달팽이 중복 제거

--- programmers/src/programmers/Q02_lv2_삼각달팽이.java
+++ programmers/src/programmers/Q02_lv2_삼각달팽이.java
@@ -1,14 +1,18 @@
 package programmers;
 
 public class Q02_lv2_삼각달팽이 {
+	private static final int[] dx = {0, 1, -1};
+	private static final int[] dy = {1, 0, -1};
+	
 	public int[] solution(int n) {
 		int[][] triangle = new int[n][n];
 		int value = 1;
 		int x = 0;
 		int y = 0;
+		int d = 0; // 방향 변수
 		
 		// triangle 채우기
-		while(true) {
+		/* while(true) {
 			// 아래로 이동
 			while(true) {
 				triangle[y][x] = value++;
@@ -40,6 +44,23 @@ public class Q02_lv2_삼각달팽이 {
 			}
 			if(y+1 == n || triangle[y+1][x] != 0) break;
 			y += 1;
+		} */
+		
+		// 중복 코드 제거
+		while(true) {
+			triangle[y][x] = value++;
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			
+			if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+				d = (d+1)%3;
+				nx = x + dx[d];
+				ny = y + dy[d];
+				
+				if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;   
+			}
+            x = nx;
+			y = ny;
 		}
 
         int[] answer = new int[value - 1];
