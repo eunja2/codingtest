f16885a10c483d5506d2fec26439a9b08802e430
eunja2 <ej0514@hanmail.net>
2023-07-03 12:22:04 +0900

    Q26 문자열 내림차순으로 배치하기, Q27 문자열 내 마음대로 정렬하기

---
+++ programmers/src/programmers/Q27_lv1_문자열내마음대로정렬하기.java
@@ -0,0 +1,16 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Q27_lv1_문자열내마음대로정렬하기 {
+	public String[] solution(String[] strings, int n) {
+		Arrays.sort(strings, (s1, s2) -> {
+			if(s1.charAt(n) != s2.charAt(n)) {
+				return s1.charAt(n) - s2.charAt(n);
+			} else
+				return s1.compareTo(s2);
+		});
+		
+        return strings;
+    }
+}
