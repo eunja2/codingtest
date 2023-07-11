def15ff141b225400b88e87ce37cc40b72a444ff
eunja2 <ej0514@hanmail.net>
2023-07-11 12:04:47 +0900

    Q43 다른 문제풀이 추가

--- programmers/src/Q43_lv2_괄호회전하기.java
+++ programmers/src/Q43_lv2_괄호회전하기.java
@@ -4,7 +4,7 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Q43_lv2_괄호회전하기 {
-	Stack<Character> stack = new Stack<>();
+	/* Stack<Character> stack = new Stack<>();
 	Queue<Character> queue = new LinkedList<>();
 	
     public int solution(String s) {
@@ -37,6 +37,40 @@ public class Q43_lv2_괄호회전하기 {
     
     private boolean isCorrect(String s) {
     	for(char c : s.toCharArray()) {
+    		switch(c) {
+    			case '(': 
+    				stack.push(')'); break;
+				case '[': 
+					stack.push(']'); break;
+				case '{':
+					stack.push('}'); break;
+    			case ')': case ']': case '}': 
+    				if(stack.isEmpty()) return false;
+    				if(stack.pop()!=c) return false;;
+    				break;
+    		}
+    	}
+    	return stack.isEmpty();
+    } */
+	
+	public int solution(String s) {
+    	char[] str = s.toCharArray();
+    	
+    	int answer = 0;
+    	for(int offset=0;offset<str.length;offset++) {
+    		if(isCorrect(str, offset))
+    			answer++;
+    	}
+    	
+        return answer;
+    }
+	
+	private boolean isCorrect(char[] str, int offset) {
+		Stack<Character> stack = new Stack<>();
+		
+    	for(int i=0;i<str.length;i++) {
+    		char c = str[(offset+i)%str.length]; // 문자열을 회전한 것과 같은 검사 수행 가능
+    		
     		switch(c) {
     			case '(': 
     				stack.push(')'); break;
