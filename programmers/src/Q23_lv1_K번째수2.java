1719d9fcb89b800eb0b8e22cd6c9f748574a1026
eunja2 <ej0514@hanmail.net>
2023-08-13 22:52:01 +0900

    Q23 K번째수

---
+++ programmers/src/Q23_lv1_K번째수2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Q23_lv1_K번째수2 {
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		
+		for(int i=0;i<commands.length;i++) {
+			int[] arrCopy = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+			Arrays.sort(arrCopy);
+			answer[i] = arrCopy[commands[i][2]-1];
+		}
+		
+        return answer;
+    }
+}
