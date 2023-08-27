67e1757861519343f69ce97530a7757bc11270b3
eunja2 <ej0514@hanmail.net>
2023-08-27 22:04:46 +0900

    Q28 가장 큰 수

---
+++ programmers/src/Q28_lv2_가장큰수2.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Q28_lv2_가장큰수2 {
+	public String solution(int[] numbers) {
+		return Arrays.stream(numbers)
+				.mapToObj(String::valueOf) // 문자열로 변환
+				.sorted((s1, s2) -> {
+					int original = Integer.parseInt(s1+s2);
+					int reversed = Integer.parseInt(s2+s1);
+					return reversed - original; // 음수면 s2가 더 크다는 의미로 순서 바꿈 
+				})
+				.collect(Collectors.joining(""))
+				.replaceAll("^0+","0"); // 0만으로 구성되었을 때
+    }
+}
